5c6e7a6842391e4b2c0ea44e2572ed2e06341092
EcceEcce <163897149+gulere@users.noreply.github.com>
2024-06-14 18:29:13 +0300

    Add files via upload

---
+++ data.py
@@ -0,0 +1,32 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+             "you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, "
+             "you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+    {"text": "A few ounces of chocolate can kill a small dog.", "answer": "True"},
+    {"text": "The music group Daft Punk got their name from a negative review they received.", "answer": "True"},
+    {"text": "Romanian belongs to the Romance language family, shared with French, Spanish, Portuguese and Italian.",
+     "answer": "True"},
+    {"text": "Activision created Battlefield 1.", "answer": "False",},
+    {"text": "Bill Cipher in the show \'Gravity Falls\' is the good guy.", "answer": "False"},
+    {"text": "\'HTML\' stands for Hypertext Markup Language.", "answer": "True"},
+    {"text": "IMAX stands for Image Maximum.", "answer": "True"},
+    {"text": "The names of Tom Nook's cousins in the Animal Crossing franchise are named \'Timmy\' and \'Jimmy\'.",
+     "answer": "False"},
+    {"text": "Time on Computers is measured via the EPOX System.", "answer": "False"},
+    {"text": "\'Elementary, my dear Watson\' "
+             "is a phrase that is never truly said within the Conan Doyle books of Sherlock Holmes.",
+     "answer": "True"},
+    {"text": "In Heroes of the Storm, "
+             "the Cursed Hollow map gimmick requires players to kill the undead to curse the enemy team.",
+     "answer": "False"}
+]
